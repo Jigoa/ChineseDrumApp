37d760cc871a5c076b1ab2eb7999324b5f738ac9
Steven Tran <steventran1997@hotmail.com>
2018-08-08 00:00:59 +0200

    tried to use circle buttons, but does not work

--- app/src/main/java/com/example/steventran/chinesedrumsapp/DrumActivity.java
+++ app/src/main/java/com/example/steventran/chinesedrumsapp/DrumActivity.java
@@ -1,13 +1,61 @@
 package com.example.steventran.chinesedrumsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class DrumActivity extends AppCompatActivity {
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class DrumActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drum);
+
+        //Side Sound
+        Button side = (Button) this.findViewById(R.id.sidedrumbutton);
+        final MediaPlayer sidesound = MediaPlayer.create(this, R.raw.side_big_sound);
+        side.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View v) {
+                sidesound.start();
+            }
+        });
+
+        //big drum sound
+        Button big = (Button) this.findViewById(R.id.bigdrumbutton);
+        final MediaPlayer bigsound = MediaPlayer.create(this, R.raw.big_drum_sound);
+        side.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View v) {
+                bigsound.start();
+            }
+        });
+
+        //medium drum sound
+        Button medium = (Button) this.findViewById(R.id.mediumdrumbuttonbutton);
+        final MediaPlayer mediumsound = MediaPlayer.create(this, R.raw.medium_drum_sound);
+        side.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View v) {
+                mediumsound.start();
+            }
+        });
+
+        //small drum sound
+        Button small = (Button) this.findViewById(R.id.smalldrumbutton);
+        final MediaPlayer smallsound = MediaPlayer.create(this, R.raw.small_drum_sound);
+        side.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View v) {
+                smallsound.start();
+            }
+        });
+
     }
+
+
 }
